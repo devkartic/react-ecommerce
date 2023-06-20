43c7472df9e692405fd7024fca3eed075abf4490
devkartic <dev.kartic@gmail.com>
2023-06-20 23:54:10 +0600

    Working with services and trusted components

--- src/About.js
+++ src/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import HeroSection from "./components/HeroSection";
 
 const About = () => {
+    const myData = {name: 'Thapa Ecommerce'};
     return (
-        <div>About</div>
+        <HeroSection myData={myData}/>
     );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import HeroSection from './components/HeroSection';
+import Services from './components/Services';
+import Trusted from './components/Trusted';
 
 const Home = () => {
+    const myData = {name: 'Thapa Store'};
     return (
-        <Wrapper className='test'>Home</Wrapper>
+        <>
+        <HeroSection myData={myData}/>
+        <Services/>
+        <Trusted/>
+        </>
     );
 }
-
-const Wrapper = styled.section`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.bg};
-`;
 
 export default Home;
